Freireg/meta-torizon-bsp-proto

commit 0a03fac50b55c9e9859c4b58c1a827dde920c2c3

linux-toradex-mainline: fix a wrong inter-task dependency
do_kernel_checkout and do_symlink_kernsrc are both modifying ${S}, they
could conflict with eacher other, move do_kernel_checkout after
do_symlink_kernsrc could fix that.

Related-to: TOR-694
Signed-off-by: Ming Liu <ming.liu@toradex.com>

recipes-kernel/linux/linux-toradex-mainline_5.4.bb
@@ -4,6 +4,18 @@ PV = "${LINUX_VERSION}+git${SRCPV}"
 require recipes-kernel/linux/linux-toradex-mainline.inc
 require recipes-kernel/linux/linux-yocto.inc
 
+python __anonymous () {
+    # A temporary fix for a inter-task dependency issue in kernel-yocto.bbclass, in
+    # which, kernel_checkout should run after do_symlink_kernsrc rather than do_unpack.
+    #
+    # A patch has been sent to OE mail list on 2020-03-04, reference:
+    # http://lists.openembedded.org/pipermail/openembedded-core/2020-March/293911.html
+    #
+    # We need this temproary hack here until that patch merged in OE layer.
+    bb.build.deltask('do_kernel_checkout', d)
+    bb.build.addtask('do_kernel_checkout', 'do_kernel_metadata', 'do_symlink_kernsrc', d)
+}
+
 SRCREV_machine = "4d9a701d8bda68d6dabcb3e05e52fbf4d3ea54bc"
 SRCREV_meta = "bc3a6a8b0bd0cd5e08cbf655227327f222fccf90"
 
@@ -21,4 +33,3 @@ SRC_URI += " \
     git://${KMETAREPOSITORY};protocol=${KMETAPROTOCOL};type=kmeta;name=meta;branch=${KMETABRANCH};destsuffix=${KMETA} \
     file://0001-Revert-ARM-dts-imx7-colibri-aster-enable-Atmel-multi.patch \
 "
-

commit 202e126658ca9f63bf34f10a72c1f93b2df89735

linux-toradex-mainline: fix a wrong inter-task regression
A regression was introduced by commit 0a03fac50:
[ linux-toradex-mainline: fix a wrong inter-task dependency ]

We dont have to delete the task do_kernel_checkout, that somehow causes
it could not be re-added in, this probably is a bitbake issue. But as
a workaround, we could drop the bb.build.deltask line, just calling
bb.build.addtask with the correct inter-task dependency would be fine.

Related-to: TOR-694
Signed-off-by: Ming Liu <ming.liu@toradex.com>

recipes-kernel/linux/linux-toradex-mainline_5.4.bb
@@ -12,7 +12,6 @@ python __anonymous () {
     # http://lists.openembedded.org/pipermail/openembedded-core/2020-March/293911.html
     #
     # We need this temproary hack here until that patch merged in OE layer.
-    bb.build.deltask('do_kernel_checkout', d)
     bb.build.addtask('do_kernel_checkout', 'do_kernel_metadata', 'do_symlink_kernsrc', d)
 }
 